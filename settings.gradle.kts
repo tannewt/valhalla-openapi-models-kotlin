2211947e7f6a12dec8c27b6b88088635e6b7fa35
Jacob Fielding <jf0517@gmail.com>
2024-10-15 22:16:11 -0700

    Designated config target

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 rootProject.name = "valhalla-models"
 
 gradle.beforeProject {
-    extensions.extraProperties["libraryVersion"] = "0.0.7"
+    extensions.extraProperties["libraryVersion"] = "0.0.8"
 }
 
 include("client")
